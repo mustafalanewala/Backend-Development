1cf44900a75cb2c7250abfc01100a47ea9e1d679
Mustafa Lanewala <125785421+mustafalanewala@users.noreply.github.com>
2025-01-05 12:40:19 +0530

    created user controller and user route, wrote user register logic and fixed some bugs

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit:"20kb"}))
-app.use(express.urlencoded({extended: true, limit:"20kb"}))
-app.use(express.static("public"))
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-app.use(cookieParser())
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declarations
+app.use("/api/v1/users", userRouter);
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
 connectDB()
-.then(() => {
+  .then(() => {
     app.listen(process.env.PORT || 8000, () => {
       console.log(`\nServer is running on port ${process.env.PORT}`);
     });
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log("Mongo DB connection failed: ", err);
-});
+  });
