d7295a3300ebc14b57b36447cef33c9444d71f13
Mustafa Lanewala <125785421+mustafalanewala@users.noreply.github.com>
2025-01-06 23:28:42 +0530

    refactor user registration to handle cover image upload and update import paths in middleware

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,8 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     // file is uploaded successfully
-    console.log("File uploaded successfully", response.url);
+    // console.log("File uploaded successfully", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     // remove locally saved temporary
